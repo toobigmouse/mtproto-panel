fa14a7430c47e75bbde0b2ed6f73fef3268d2cc5
Daniel <128586031+danielVNru@users.noreply.github.com>
2026-04-06 21:00:55 +0300

    refactor(frontend): хуки, SCSS-модули, реорганизация страниц по папкам
    - Выделены общие утилиты: компонент FlagIcon, formatBytes, buildChartOptions/buildChartData
    - Логика страниц вынесена в кастомные хуки:
      useLogin, useNodes, useNodeDetail, useProxies, useProxyDetail
    - Инлайн-стили и глобальные CSS-классы заменены SCSS-модулями
      для каждой страницы и компонента:
      Login, Nodes, NodeDetail, Proxies, ProxyDetail, Settings, ProxyCard
    - App.css сокращён до базовых глобальных стилей
    - Каждая страница перенесена в отдельную папку (Login/, Nodes/ и т.д.)
      с co-located .tsx и .module.scss файлами
    - В каждой папке страницы добавлен barrel-файл index.ts для чистых импортов

---
+++ frontend/src/pages/Login.tsx
@@ -0,0 +1,51 @@
+import { Button, TextInput, Card, Alert } from '@gravity-ui/uikit';
+import { useLogin } from '../hooks/useLogin';
+import s from './Login.module.scss';
+
+export default function Login() {
+  const { username, setUsername, password, setPassword, error, loading, handleSubmit } = useLogin();
+
+  return (
+    <div className={s.container}>
+      <Card className={s.card} view="outlined">
+        <h2>MTProto Panel</h2>
+        <form onSubmit={handleSubmit}>
+          {error && (
+            <div className={s.errorWrap}>
+              <Alert theme="danger" message={error} />
+            </div>
+          )}
+          <div className={s.field}>
+            <label>Имя пользователя</label>
+            <TextInput
+              value={username}
+              onUpdate={setUsername}
+              placeholder="Введите имя"
+              size="l"
+              autoFocus
+            />
+          </div>
+          <div className={s.field}>
+            <label>Пароль</label>
+            <TextInput
+              value={password}
+              onUpdate={setPassword}
+              type="password"
+              placeholder="Введите пароль"
+              size="l"
+            />
+          </div>
+          <Button
+            type="submit"
+            view="action"
+            size="l"
+            width="max"
+            loading={loading}
+          >
+            Войти
+          </Button>
+        </form>
+      </Card>
+    </div>
+  );
+}
